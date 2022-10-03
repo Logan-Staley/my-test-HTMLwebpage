c9b0c8adaff57f4f2f505c8cedfe35318cea6dc8
Logan-Staley <92904400+Logan-Staley@users.noreply.github.com>
2022-10-03 15:46:11 -0500

    commit

--- vue project/weatherapp/src/App.vue
+++ vue project/weatherapp/src/App.vue
@@ -5,7 +5,7 @@
   <div id="app">
     <main>
       <div class = "search-box">
-
+        <input type="text" class="search-bar" placeholder="Search..."/>
       </div>
     </main>
   </div>
@@ -34,4 +34,43 @@ export default {
   margin-top: 60px;
 }
 */
+*{
+  margin:0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body{
+  font-family: "montserrant", sans-serif;
+}
+#app{
+  background-image: url('./assets/background.jpg');
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s;
+}
+main{
+  min-height: 100vh;
+  padding: 25px;
+  background-image: linear-gradient(to bottom, rgba(0,0,0,.25),rgba(0,0,0,.75));
+}
+.search-box{
+  width: 100%;
+  margin-bottom: 30px;
+}
+.search-box .search-box{
+  display:black;
+  width:100%;
+  padding: 15px;
+  color: #313131;
+  font-size:20px;
+  
+  appearance: none;
+  border:none;
+  outline: none;
+  background: none; 
+  
+  background-color: rgba(255,255,255,0);
+
+}
+
 </style>
